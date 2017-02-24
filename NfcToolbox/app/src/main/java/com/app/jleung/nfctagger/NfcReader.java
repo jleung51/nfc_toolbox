706c0f1db2d998a1b8b9a256f36ef825e5cf3ac8
Jeffrey Leung <jleung513@gmail.com>
2017-02-23 22:54:43 -0800

    Improve usability of NfcReader
    Logs and more error information have been added.

--- NfcToolbox/app/src/main/java/com/app/jleung/nfctagger/NfcReader.java
+++ NfcToolbox/app/src/main/java/com/app/jleung/nfctagger/NfcReader.java
@@ -5,17 +5,21 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
+import android.util.Log;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 public class NfcReader {
 
+    private static final String TAG = NfcReader.class.getSimpleName();
+
     private static final String MIME_TEXT_PLAIN = "text/plain";
 
     public String readTagFromIntent(Intent intent) throws NfcReadFailureException {
         // No tag when you startup the application
         if(intent.getAction().equals("android.intent.action.MAIN")) {
+            Log.d(TAG, "Startup Intent detected - no action taken");
             return null;
         }
         else if(!NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
@@ -29,23 +33,21 @@ public class NfcReader {
     }
 
     private String readTag(Tag tag) throws NfcReadFailureException {
-        Ndef ndef = Ndef.get(tag);
-        if(ndef == null) {
-            return null;
+        Ndef ndefTag = Ndef.get(tag);
+        if(ndefTag == null) {
+            throw new NfcReadFailureException(NfcReadFailureException.Reason.INVALID_TAG_TYPE);
         }
 
         NdefRecord extractedRecord = null;
-        NdefRecord[] ndefRecords = ndef.getCachedNdefMessage().getRecords();
-        for(NdefRecord ndefRecord : ndefRecords) {
+        for(NdefRecord ndefRecord : ndefTag.getCachedNdefMessage().getRecords()) {
             if(ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN &&
                     Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
                 extractedRecord = ndefRecord;
                 break;
             }
         }
-
         if(extractedRecord == null) {
-            return null;
+            return "";
         }
 
         return readTagText(extractedRecord);
@@ -60,12 +62,15 @@ public class NfcReader {
      * @throws UnsupportedEncodingException
      */
     private String readTagText(NdefRecord ndefRecord) throws NfcReadFailureException {
+        Log.d(TAG, "Reading NDEF record from tag payload");
+
         byte[] payload = ndefRecord.getPayload();
         int languageCodeLength = payload[0] & 0063;
         String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
 
+        String data;
         try {
-            return new String(
+            data = new String(
                     payload,
                     languageCodeLength + 1,
                     payload.length - languageCodeLength - 1,
@@ -75,6 +80,9 @@ public class NfcReader {
         catch(UnsupportedEncodingException e) {
             throw new NfcReadFailureException(NfcReadFailureException.Reason.INVALID_ENCODING);
         }
+
+        Log.d(TAG, "Parsed tag data: [" + data + "]");
+        return data;
     }
 
     public static class NfcReadFailureException extends Exception {
